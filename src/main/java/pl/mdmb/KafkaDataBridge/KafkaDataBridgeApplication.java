fbe0f586c1fcdb4cbc82256771adf6e3bd5ed791
Mikolaj Bogudal <mbogudal@gmail.com>
2025-12-11 21:42:58 +0100

    working sending messages

--- src/main/java/pl/mdmb/KafkaDataBridge/KafkaDataBridgeApplication.java
+++ src/main/java/pl/mdmb/KafkaDataBridge/KafkaDataBridgeApplication.java
@@ -2,6 +2,7 @@ package pl.mdmb.KafkaDataBridge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
